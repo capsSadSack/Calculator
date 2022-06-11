f5644bd4986d665b04e7bf494a26cf5113fe0ad6
capsSadSack <cherkashin@phystech.edu>
2022-06-11 13:37:36 +0300

    Dial view
    - view sketch
    - properties binding

---
+++ Calculator/CalculatorWPF/ViewModels/DialViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorWPF.ViewModels
+{
+    internal class DialViewModel : INotifyPropertyChanged
+    {
+        private string _firstText = "0";
+        public string FirstText 
+        {
+            get => _firstText;            
+            set
+            {
+                _firstText = value;
+                OnPropertyChanged("FirstText");
+            }
+        }
+
+        private string _operationText = "";
+        public string OperationText
+        {
+            get => _operationText;
+            set
+            {
+                _operationText = value;
+                OnPropertyChanged("OperationText");
+            }
+        }
+
+        private string _secondText = "";
+        public string SecondText
+        {
+            get => _secondText;
+            set
+            {
+                _secondText = value;
+                OnPropertyChanged("SecondText");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
